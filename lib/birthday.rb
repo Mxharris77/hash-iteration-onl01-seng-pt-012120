9f943f0a0215afa7eed58c0fe8bb7ecd98bbc023
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-04 00:58:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,8 +15,8 @@ def age_approptiate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
    if age < 13
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  else 
-    puts You are too old for this.""
+  else ""
+    puts You are too old for this.
   end
 end 
 
